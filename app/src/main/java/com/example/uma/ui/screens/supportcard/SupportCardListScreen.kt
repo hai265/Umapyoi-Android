6682b2869b724264e45df02f0f12aa55e26bd199
Hai Nguyen <cognation100@gmail.com>
2025-12-10 15:28:10 -0500

    network titleEn nullable, TODO LaunchedEffect grid scroll 0

--- app/src/main/java/com/example/uma/ui/screens/supportcard/SupportCardListScreen.kt
+++ app/src/main/java/com/example/uma/ui/screens/supportcard/SupportCardListScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
@@ -20,9 +21,16 @@ fun SupportCardListScreen(modifier: Modifier = Modifier, onTapSupportCard: (Int)
     val umaListState by viewModel.uiState.collectAsState()
     val gridState = rememberLazyGridState()
 
+    // Scroll to top of list when list changes (from search, etc.)
+    //TODO: This now causes a bug where if a user backs out from the support card detailed page the list scrolls back to the first position
+    //TODO: Also refactor this and list to a separate component
+    LaunchedEffect(umaListState.list) {
+        gridState.scrollToItem(0)
+    }
+
     ScreenWithSearchBar(
         textFieldState = viewModel.searchTextBoxState,
-        onRefresh = {viewModel.refreshList()},
+        onRefresh = { viewModel.refreshList() },
         contentEmpty = umaListState.list.isEmpty(),
         searchBoxLabel = "Search Support Card",
         syncing = umaListState.syncing
