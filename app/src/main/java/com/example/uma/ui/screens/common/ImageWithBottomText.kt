a21a97feddb6b7e6bb862ce97c3a23ab566be96c
Hai Nguyen <cognation100@gmail.com>
2025-12-10 18:26:32 -0500

    image fill card, TODO: add colors

--- app/src/main/java/com/example/uma/ui/screens/common/ImageWithBottomText.kt
+++ app/src/main/java/com/example/uma/ui/screens/common/ImageWithBottomText.kt
@@ -1,17 +1,20 @@
 package com.example.uma.ui.screens.common
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
@@ -29,7 +32,21 @@ fun ImageWithBottomText(
     onClickImage: () -> Unit,
     bottomText: String,
     imageUrl: String,
+    //TODO: Change to empty, add colors to network
+    primaryColorHex: String = "#EE6DCB",
+    secondaryColorHex: String = "#FFDEF9",
 ) {
+    val primaryColor = try {
+        Color(android.graphics.Color.parseColor(primaryColorHex))
+    } catch (e: Exception) {
+        Color.Gray
+    }
+    val secondaryColor = try {
+        Color(android.graphics.Color.parseColor(secondaryColorHex))
+    } catch (e: Exception) {
+        Color.LightGray
+    }
+
     Card {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
@@ -42,12 +59,25 @@ fun ImageWithBottomText(
                     .crossfade(true)
                     .build(),
                 contentDescription = null,
-                modifier = Modifier.size(150.dp),
                 //TODO: special week icon ONLY FOR PREVIEW
                 placeholder = painterResource(R.drawable.specialweek_icon),
                 //TODO: Only for preview delete when done
-                error = painterResource(R.drawable.specialweek_icon)
+                error = painterResource(R.drawable.specialweek_icon),
+                modifier = Modifier
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.Transparent,
+                                secondaryColor.copy(alpha = 0.3f), // Fade in secondary color
+                                primaryColor.copy(alpha = 0.7f)     // End with primary color
+                            ),
+                        )
+                    )
+                    .fillMaxWidth()
+
             )
+
 
             if (bottomText.isNotEmpty()) {
                 Box(
@@ -76,6 +106,8 @@ fun ImageWithBottomTextPreview(
     ImageWithBottomText(
         onClickImage = {},
         bottomText = "Special Weekfwowefofwjeiewfoijweiofwej",
-        imageUrl = ""
+        imageUrl = "",
+        primaryColorHex = "#EE6DCB",
+        secondaryColorHex = "#FFDEF9"
     )
 }
